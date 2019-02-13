9408fa8bc824d637f85f7d5fd264158ff56f21cd
Glitch (sivonne-inclass-glitch-demo-1) <none>
2019-02-13 02:40:17 +0000

    🌻📷 Checkpoint ./public/client.js:3109007/4418

--- public/client.js
+++ public/client.js
@@ -80,35 +80,42 @@ $(function() {
     console.log(data);
     console.groupEnd();
     
-    // // Display the artist name
-    // var artists = '';
-    // //for more than one artist
-    // data.artists.forEach(function(element){
-    //  //console.log("Loop:" + element); 
-    //  artists = artists + element.name + '';
+    // Display the artist name
+    var artists = '';
+    var img = $('<img class="circle-image" />');
+    //for more than one artist
+    data.artists.forEach(function(element){
+     artists = artists + element.name + '';
+    });
+    
+    var artistName = $('<h5>' + data.artists[0].name + data.artists[1].name  + '</h5>');
+    artistName.appendTo('#artist-container');
+    
+    // img.appendTo('#artist-container');
+    var trackName = $('<h3>' + data.name + '</h3>');
+    data.artists.forEach(function(element){
+    // trackName.appendTo('#artist-container');
+      });
+    // data.genres.map(function(genre, i) {
+    // var genreItem = $('<p>' + genre + '</p>');
+    // genreItem.appendTo('#artist-container');
     // });
     
-//     var artistName = $('<h5>' + data.artists[0].name + data.artists[1].name  + '</h5>');
-//     artistName.appendTo('#artist-container');
     
+//     // Display the artist's image
+//     var img = $('<img class="circle-image" />');
+//     img.attr('src', data.images[0].url);
+//     img.appendTo('#artist-container');
     
-    
-var artists = '';
-var img = $('<img class="circle-image" />');
-var trackName = $('<h3>' + data.name + '</h3>');
-var genreItem = $('<p>' + genre + '</p>');
- 
-    data.artists.forEach(function(element){
-    img.attr('src', data.images[0].url);
-    img.appendTo('#artist-container');
-    
+//     // Display the artist name
+//     var trackName = $('<h3>' + data.name + '</h3>');
     trackName.appendTo('#artist-container');
-    data.genres.map(function(genre, i) {
-       genreItem.appendTo('#artist-container');
-    }
-                    
-                    }
-                    });
+    
+//     // Display the artist's genres
+//     data.genres.map(function(genre, i) {
+//       var genreItem = $('<p>' + genre + '</p>');
+//       genreItem.appendTo('#artist-container');
+//     });
   });
   
   
